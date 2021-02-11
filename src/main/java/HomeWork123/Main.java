817419b7ab9087e98c5c7b7e69b62af7d5112f12
Alexander <pancasancha4@gmail.com>
2021-02-11 18:55:35 +0300

    created class with two methods for create random array and to swap elements of array

--- src/main/java/HomeWork123/Main.java
+++ src/main/java/HomeWork123/Main.java
@@ -1,3 +1,5 @@
+package HomeWork123;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
---
+++ src/main/java/HomeWork4/ReplaceArray.java
@@ -0,0 +1,41 @@
+package HomeWork4;
+
+/**
+ * @author Alexander Krasnov
+ * @see #createArray() which create a new array with length 20 and random elements (numbers -10 up to +10 )
+ * @see #replaceMinMax(int[]) which swaps the max negative array element and min positive
+ */
+public class ReplaceArray {
+    public ReplaceArray(){
+        replaceMinMax(createArray());
+    }
+
+    private static int[] createArray() {
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 22 - 11);
+        }
+        return array;
+    }
+
+    private static int[] replaceMinMax(int[] arr) {
+        int min = arr[0];
+        int max = arr[0];
+        int indexMin = 0;
+        int indexMax = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0 & arr[i] < min) {
+                min = arr[i];
+                indexMin = i;
+            }
+            if (arr[i] > 0 & arr[i] < max) {
+                max = arr[i];
+                indexMax = i;
+            }
+            arr[indexMin] = max;
+            arr[indexMax] = min;
+        }
+        return arr;
+    }
+
+}
